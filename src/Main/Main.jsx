25790e1f915d253f3f60911abea480cb5736ed15
MD.MARUF SHEIKH <marufsheikh803@gmail.com>
2025-11-01 00:43:59 +0600

    new file added

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Navber from '../components/Navber';
+import Welcome from '../Pages/Welcome/Welcome';
 
 const Main = () => {
     return (
         <div className='container px-5 mx-auto'>
             <Navber></Navber>
+            <div className='my-6'>
+                <Welcome></Welcome>
+            </div>
         </div>
     );
 };
