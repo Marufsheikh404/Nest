8439519589823369c8061d82511827a7b120a5aa
MD.MARUF SHEIKH <marufsheikh803@gmail.com>
2025-11-01 01:42:24 +0600

    new file added

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Navber from '../components/Navber';
 import Welcome from '../Pages/Welcome/Welcome';
 
 const Main = () => {
+    const [image,setImage] = useState([]);
+
+    useEffect(()=>{
+        fetch('/public/Image.json')
+        .then(res => res.json())
+        .then(data => setImage(data))
+    },[])
+
     return (
         <div className='container px-5 mx-auto'>
             <Navber></Navber>
-            <div className='my-6'>
-                <Welcome></Welcome>
+            <div className='my-6 container px-8'>
+                <Welcome image ={image}></Welcome>
             </div>
         </div>
     );
--- src/Pages/Welcome/Welcome.jsx
+++ src/Pages/Welcome/Welcome.jsx
@@ -1,8 +1,39 @@
 
+import picture from '../../assets/gril.png';
 
-const Welcome = () => {
+const Welcome = ({image}) => {
+
     return (
-        <div>
+        <div className='flex gap-6 flex-col md:flex-row items-start p-6'>
+            {/* Left side image */}
+            <img className='w-96 rounded-2xl shadow-md' src={picture} alt="cooking" />
+
+            {/* Right side content */}
+            <div className='flex-1'>
+                <h1 className="text-[#253D4E] text-5xl font-bold mb-4">Welcome to Nest</h1>
+
+                <div className="space-y-4 mb-6">
+                    <p className="text-[#7E7E7E]">
+                        Ius ferri velit sanctus cu, sed at soleat accusata. Dictas prompta et Ut placerat legendos interpre.Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante Etiam sit amet orci eget. Quis commodo odio aenean sed adipiscing. Turpis massa tincidunt dui ut ornare lectus.
+                    </p>
+                    <p className="text-[#7E7E7E]">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate id est 
+                    </p>
+                </div>
+
+                {/* Image Carousel */}
+                <div className="carousel carousel-center bg-white rounded-box max-w-lg space-x-4 p-4">
+                    {image.map(pic => (
+                        <div key={pic.id} className="carousel-item">
+                            <img
+                                src={pic.imageUrl}
+                                alt={`Slide ${pic.id}`}
+                                className="w-60 h-38 object-cover"
+                            />
+                        </div>
+                    ))}
+                </div>
+            </div>
         </div>
     );
 };
