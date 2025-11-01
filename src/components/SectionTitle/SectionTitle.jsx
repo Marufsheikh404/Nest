12409384d38cddf00f7d2827b25a36b2c2b6986f
MD.MARUF SHEIKH <marufsheikh803@gmail.com>
2025-11-01 12:16:54 +0600

    card is done

--- src/components/SectionTitle/SectionTitle.jsx
+++ src/components/SectionTitle/SectionTitle.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const SectionTitle = ({title}) => {
     return (
         <div>
-            <h1 className='text-5xl font-semibold text-[#253D4E]'>{title}</h1>
+            <h1 className='text-5xl font-bold text-[#253D4E] text-center my-6'>{title}</h1>
         </div>
     );
 };
